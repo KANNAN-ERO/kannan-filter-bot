81af3e52e87c90d5fc9f4ab5c15a54fd2038e049
KANNAN-ERO <97578723+KANNAN-ERO@users.noreply.github.com>
2022-07-26 11:07:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/KANNAN-ERO/kannan-filter-bot.git /kannanbot 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /kannanbot
 fi
-cd /EvaMaria
+cd /kannanbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
